472c04ca41d86e182845284c45e3295b35405096
Clément Walter <clement0walter@gmail.com>
2026-02-24 21:50:53 +0200

    pull latest stwo and pin version

--- crates/stwo/src/prover/backend/cpu/merkle_lifted.rs
+++ crates/stwo/src/prover/backend/cpu/merkle_lifted.rs
@@ -52,7 +52,7 @@ impl<H: MerkleHasherLifted> MerkleOpsLifted<H> for CpuBackend {
         assert!(columns[0].len() >= 2, "A column must be of length >= 2.");
         let mut prev_layer: Vec<H> = vec![hasher; 2];
         let mut prev_layer_log_size: u32 = 1;
-        for (log_size, group) in columns.iter().group_by(|c| c.len().ilog2()).into_iter() {
+        for (log_size, group) in columns.iter().chunk_by(|c| c.len().ilog2()).into_iter() {
             let log_ratio = log_size - prev_layer_log_size;
             prev_layer = (0..1 << log_size)
                 // We only clone when starting a column chunk of different size.
